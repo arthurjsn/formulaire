21b8b31a4ae0edf2ad70c99866c78f2d63992a92
Arthur <arthur@jasinski.fr>
2021-11-22 16:01:07 +0100

    ajout formulaire

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Toggle/>
+</template>
+
+<script>
+import Toggle from './components/Toggle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Toggle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
